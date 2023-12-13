ab1d39985587024125335faf07db7fe1aca6094b
Muhammad Husnain <150600466+Muhammad-Husnain007@users.noreply.github.com>
2023-12-13 23:12:20 +0500

    Add files via upload

---
+++ chapter 43 to 48/app.js
@@ -0,0 +1,58 @@
+// 1. Show an alert box on click on a link.
+
+// function showAlert(){
+// alert('Welcome to alert box !')
+// }
+
+// function shoMsg(){
+//     alert('Thanks for purchacing the form from us!')
+//     }
+
+// 3. Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be deleted.
+
+
+
+// function dltName(arr){
+        
+// var a = document.getElementById('btn')
+// var b = a.remove()
+// var c = arr.remove()
+
+
+//         }
+// var arr = ['hasnain','khan','adnan','khan']
+// arr.slice(0,3)
+// document.write(arr.join('  '))
+
+
+// // dltName()
+
+// // var rem = arr.slice
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
